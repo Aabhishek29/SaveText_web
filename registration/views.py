3130b58ca93f7f0f00261a25fed9ccc4bbe34f10
Esha Gusain <eshgsn@gmail.com>
2022-03-25 23:21:30 +0530

    Implemented textArea Page

--- registration/views.py
+++ registration/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+
+def getUserLogin(request):
+    return render(request,'textUtils.html')
